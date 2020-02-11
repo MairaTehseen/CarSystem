e225fc7f832ae57e4e23fb633b0ef5fe5aaaabf3
p2425524 <p2425524@GWHXX472WIN001W>
2020-02-11 14:40:29 +0000

    Test 2

---
+++ Car Testing/tstStock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Car_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock AStock = new clsStock();
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
