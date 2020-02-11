3fa40d3b1efaf741ea396a7d08dfd4313cd3c682
alyssambp <99alyssapagulayan@gmail.com>
2020-02-11 15:18:03 +0000

    created tstCustomer

---
+++ Car Testing/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CarClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Car_Testing
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance oif the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
