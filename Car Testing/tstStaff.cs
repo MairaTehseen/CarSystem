e3520a2a38849c045685db36e8f127e1900ce17a
p2433964 <p2433964@GWHXX472WIN010W>
2020-02-18 14:47:06 +0000

    i have created a test class for staff

---
+++ Car Testing/tstStaff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Car_Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
