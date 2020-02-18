3e5f959a1be671cb66388b273bb285c93a8f49e4
p2425524 <p2425524@my365.dmu.ac.uk>
2020-02-18 15:45:51 +0000

    Lots of testing

--- Car Testing/tstStock.cs
+++ Car Testing/tstStock.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CarClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Car_Testing
@@ -12,5 +13,104 @@ namespace Car_Testing
             clsStock AStock = new clsStock();
             Assert.IsNotNull(AStock);
         }
+
+
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+            Boolean TestData = true;
+            AStock.Active = TestData;
+            Assert.AreEqual(AStock.Active, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsStock AStock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            AStock.DateAdded = TestData;
+            Assert.AreEqual(AStock.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void StockNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.StockNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.StockNo, TestData);
+        }
+
+        [TestMethod]
+        public void CarNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.CarNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.CarNo, TestData);
+        }
+
+        [TestMethod]
+        public void ManufacturerNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "BMW";
+            //assign the data to the property
+            AStock.Manufacturer = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Manufacturer, TestData);
+        }
+
+        [TestMethod]
+        public void MakePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "1 Series";
+            //assign the data to the property
+            AStock.Make = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Make, TestData);
+        }
+
+        [TestMethod]
+        public void ColourPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "Black";
+            //assign the data to the property
+            AStock.Colour = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Colour, TestData);
+        }
+
+        [TestMethod]
+        public void TownPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 15000;
+            //assign the data to the property
+            AStock.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Price, TestData);
+        }
     }
 }
+
---
+++ CarClasses/clsStock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarClasses
+{
+    public class clsStock
+    {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int StockNo { get; set; }
+        public int CarNo { get; set; }
+        public string Manufacturer { get; set; }
+        public string Make { get; set; }
+        public string Colour { get; set; }
+        public int Price { get; set; }
+    }
+}
