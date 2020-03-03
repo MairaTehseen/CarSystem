5cc975cd21eab4f0995f0473cb4e14d4e907d169
Cezary Szwalbe <P2446634@my365.dmu.ac.uk>
2020-03-03 11:14:29 +0000

    asd

---
+++ Car Testing/tstPaymentCardsCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Car_Testing
+{
+    [TestClass]
+    public class tstPaymentCardsCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsPaymentCardsCollection Card1 = new clsPaymentCardsCollection() ;
+            Assert.IsNotNull(Card1);
+        }
+    }
+}
