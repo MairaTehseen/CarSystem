8910423e47980d7638661764f0eadcc280155388
Cezary Szwalbe <P2446634@my365.dmu.ac.uk>
2020-03-03 10:47:14 +0000

    test

---
+++ Car Testing/tstPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Car_Testing
+{
+    [TestClass]
+    public class tstPayment
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsPayment aPayment = new clsPayment();
+            Assert.IsNotNull(aPayment);
+        }
+    }
+}
